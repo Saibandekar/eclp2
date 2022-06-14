51673c5cf98eb2493e24cbea45358da544719e5b
Sai Bandekar <Sai Bandekar@LAPTOP-O431PF73>
2022-06-15 01:52:16 +0530

    commit3

---
+++ CaseToolsExam/src/Caterpillar_217.java
@@ -0,0 +1,16 @@
+
+public class Caterpillar_217 {
+	
+	int moves, cabbages;
+	
+	Caterpillar_217(int moves, int cabbages) {
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
